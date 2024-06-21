59c77d29d842ecf19fc72b2d7aa8a11fc401d5c4
Hsuchimiao <s1100128@gm.pu.edu.tw>
2024-06-21 21:48:00 +0800

    Update Shioaji_KBar_MA_RSI__BollingerBand_MACD_backTest_load_Excel_and_Draw_KBar_2_plotly_chooseDates_streamlit_GitHub_6_short.py

--- Shioaji_KBar_MA_RSI__BollingerBand_MACD_backTest_load_Excel_and_Draw_KBar_2_plotly_chooseDates_streamlit_GitHub_6_short.py
+++ Shioaji_KBar_MA_RSI__BollingerBand_MACD_backTest_load_Excel_and_Draw_KBar_2_plotly_chooseDates_streamlit_GitHub_6_short.py
@@ -26,13 +26,13 @@ stc.html(html_temp)
 #df = pd.read_excel("kbars_2330_2022-07-01-2022-07-31.xlsx")
 
 # ## 讀取 excel 檔
-# df_original = pd.read_excel("kbars_2330_2022-01-01-2022-11-18.xlsx")
+# df_original = pd.read_excel("kbars_2317_2020-01-01-2024-06-20.xlsx")
 
 # ## 保存为Pickle文件:
 # df_original.to_pickle('kbars_2330_2022-01-01-2022-11-18.pkl')
 
 ## 读取Pickle文件
-df_original = pd.read_pickle('kbars_2317_2020-01-01-2024-06-20')
+df_original = pd.read_pickle('kbars_2317_2020-01-01-2024-06-20.xlsx')
 
 
 #df.columns  ## Index(['Unnamed: 0', 'time', 'open', 'low', 'high', 'close', 'volume','amount'], dtype='object')
@@ -47,9 +47,9 @@ df_original = df_original.drop('Unnamed: 0',axis=1)
 
 
 ##### 選擇資料區間
-st.subheader("選擇開始與結束的日期, 區間:2022-01-03 至 2022-11-18")
-start_date = st.text_input('選擇開始日期 (日期格式: 2022-01-03)', '2022-01-03')
-end_date = st.text_input('選擇結束日期 (日期格式: 2022-11-18)', '2022-11-18')
+st.subheader("選擇開始與結束的日期, 區間:2020-01-01 至 2024-06-20")
+start_date = st.text_input('選擇開始日期 (日期格式: 2020-01-01)', '2020-01-01')
+end_date = st.text_input('選擇結束日期 (日期格式: 2024-06-20)', '2024-06-20')
 start_date = datetime.datetime.strptime(start_date,'%Y-%m-%d')
 end_date = datetime.datetime.strptime(end_date,'%Y-%m-%d')
 # 使用条件筛选选择时间区间的数据
